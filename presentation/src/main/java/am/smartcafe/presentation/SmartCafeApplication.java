5e10fcb49b78a90c26f919cecebaa733c74b16a1
sevak.martirosyan <sevak.martirosyan@ukg.com>
2021-12-10 21:42:48 +0400

    fixed failing build, corrected versions, renamed main class.

--- presentation/src/main/java/am/smartcafe/presentation/SmartCafeApplication.java
+++ presentation/src/main/java/am/smartcafe/presentation/SmartCafeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PresentationApplication {
+public class SmartCafeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PresentationApplication.class, args);
+        SpringApplication.run(SmartCafeApplication.class, args);
     }
 
 }
