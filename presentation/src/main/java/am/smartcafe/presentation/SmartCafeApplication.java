af990db22fd9f0b5240c8e86620b8dc3686bb346
Grigor_Tumanyan <80620982+GrigorTumanyan@users.noreply.github.com>
2021-12-21 15:31:00 +0400

    added logback xml and dependency slf4j, logback

--- presentation/src/main/java/am/smartcafe/presentation/SmartCafeApplication.java
+++ presentation/src/main/java/am/smartcafe/presentation/SmartCafeApplication.java
@@ -1,10 +1,14 @@
 package am.smartcafe.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartCafeApplication {
+//    example
+    private static final Logger logger = LoggerFactory.getLogger(SmartCafeApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SmartCafeApplication.class, args);
