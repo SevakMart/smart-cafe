b81df4a897a022890c0d414a435b854d86658f5f
Grigor_Tumanyan <80620982+GrigorTumanyan@users.noreply.github.com>
2021-12-22 22:42:20 +0400

    added flyway

---
+++ data-access/src/main/resources/db/migration/V1__Add_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS user(
+id bigint not null auto_increment,
+email varchar(255),
+first_Name varchar(255),
+is_Active bit not null,
+last_Name varchar(255),
+password varchar(255),
+role INTEGER,
+primary key (id));
