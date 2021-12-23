c696e5b70c34ea0cc137b820d8ab2514fabe7329
sevak.martirosyan <sevak.martirosyan@ukg.com>
2021-12-23 11:54:10 +0400

    Revert "added flyway"
    This reverts commit b81df4a8

--- data-access/src/main/resources/db/migration/V1__Add_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user(
-id bigint not null auto_increment,
-email varchar(255),
-first_Name varchar(255),
-is_Active bit not null,
-last_Name varchar(255),
-password varchar(255),
-role INTEGER,
-primary key (id));
--- data-access/src/main/resources/db/migration/V2__Add_admin.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user (id, first_Name, last_Name, is_Active, role, email, password)
-values (1, 'admin', 'admin', true, '1', 'admin@smart-kitchen.com', 'Password1!');
